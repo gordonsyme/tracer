692e16dbe87e74101f6a2369eb5b9ced1a8f99f0
Gordon Syme <gordonsyme@gmail.com>
2019-04-25 22:11:30 +0100

    Add matrices

---
+++ src/tracer/entities/matrix.clj
@@ -0,0 +1,87 @@
+(ns tracer.entities.matrix
+  (:refer-clojure :exclude [get identity])
+  (:require [clojure.spec.alpha :as s]
+            [tracer.entities.tuple :as t]))
+
+(s/def ::row (s/and vector? (s/coll-of double?)))
+(s/def ::matrix (s/and vector?
+                       (s/coll-of ::row)))
+
+(defn matrix
+  [& rows]
+  {:pre [(= 1 (->> (map count rows)
+                   set
+                   count))]}
+   (vec (for [r rows]
+          (mapv double r))))
+(s/fdef matrix
+  :args (s/cat :rows (s/* (s/coll-of number?)))
+  :ret ::matrix)
+
+(defn identity
+  [n]
+  (let [row (vec (repeat n 0.0))]
+    (vec
+      (for [i (range n)]
+        (assoc row i 1.0)))))
+(s/fdef identity
+  :args (s/cat :n pos-int?)
+  :ret ::matrix)
+
+(defn get
+  [m i j]
+  (get-in m [i j]))
+(s/fdef get
+  :args (s/cat :m ::matrix
+               :i nat-int?
+               :j nat-int?)
+  :ret double?)
+
+(defn transpose
+  [m]
+  (vec
+    (for [i (range (count (first m)))]
+      (mapv #(clojure.core/get % i) m))))
+(s/fdef transpose
+  :args (s/cat :m ::matrix)
+  :ret ::matrix)
+
+(defn mul
+  [a b]
+  (let [b (transpose b)
+        dot (fn [a b]
+              (reduce + (map * a b)))]
+    (vec
+      (for [row a]
+        (mapv dot (repeat row) b)))))
+(s/fdef mul
+  :args (s/cat :a ::matrix
+               :b ::matrix)
+  :ret ::matrix)
+
+(defn- tuple->matrix
+  [tup]
+  (mapv vector tup))
+(s/fdef tuple->matrix
+  :args (s/cat :tup ::t/tuple)
+  :ret ::matrix)
+
+(defn- matrix->tuple
+  [mat]
+  (vec (flatten mat)))
+(s/fdef matrix->tuple
+  :args (s/cat :mat ::matrix)
+  :ret ::t/tuple)
+
+(defn mult
+  "Multiply a matrix by a tuple.
+
+  Returns a tuple."
+  [mat tup]
+  (->> (tuple->matrix tup)
+       (mul mat)
+       (matrix->tuple)))
+(s/fdef mult
+  :args (s/cat :mat ::matrix
+               :tup ::t/tuple)
+  :ret ::t/tuple)
---
+++ test/tracer/entities/matrix_test.clj
@@ -0,0 +1,136 @@
+(ns tracer.entities.matrix-test
+  (:require [clojure.test :refer (deftest testing is)]
+            [tracer.comparators :refer (approx)]
+            [tracer.fixtures :refer (instrument)]
+            [tracer.entities.tuple :as t]
+            [tracer.entities.matrix :as sut]))
+
+(clojure.test/use-fixtures :once instrument)
+
+(deftest rows-must-be-the-same-size
+  (is (thrown? AssertionError
+        (sut/matrix [ 1    2    3    4]
+                    [ 5.5  6.5  7.5]))))
+
+(deftest constructing-and-creating-a-4x4-matrix
+  (let [m (sut/matrix [ 1    2    3    4]
+                      [ 5.5  6.5  7.5  8.5]
+                      [ 9   10   11   12]
+                      [13.5 14.5 15.5 16.5])]
+    (is (== 1 (sut/get m 0 0)))
+    (is (== 4 (sut/get m 0 3)))
+    (is (== 5.5 (sut/get m 1 0)))
+    (is (== 7.5 (sut/get m 1 2)))
+    (is (== 11 (sut/get m 2 2)))
+    (is (== 13.5 (sut/get m 3 0)))
+    (is (== 15.5 (sut/get m 3 2)))))
+
+(deftest a-2x2-matrix-can-be-represented
+  (let [m (sut/matrix [-3  5]
+                      [ 1 -2])]
+    (is (== -3 (sut/get m 0 0)))
+    (is (== 5 (sut/get m 0 1)))
+    (is (== 1 (sut/get m 1 0)))
+    (is (== -2 (sut/get m 1 1)))))
+
+(deftest a-3x3-matrix-can-be-represented
+  (let [m (sut/matrix [-3  5  0]
+                      [ 1 -2 -7]
+                      [ 0  1  1])]
+    (is (== -3 (sut/get m 0 0)))
+    (is (== -2 (sut/get m 1 1)))
+    (is (== 1 (sut/get m 2 2)))))
+
+(deftest equality-with-identical-matrices
+  (let [a (sut/matrix [1 2 3 4]
+                      [5 6 7 8]
+                      [9 8 7 6]
+                      [5 4 3 2])
+        b (sut/matrix [1 2 3 4]
+                      [5 6 7 8]
+                      [9 8 7 6]
+                      [5 4 3 2])]
+    (is (= a b))
+    (is (= a a))
+    (is (approx a b))
+    (is (approx a a))))
+
+(deftest equality-with-different-matrices
+  (let [a (sut/matrix [1 2 3 4]
+                      [5 6 7 8]
+                      [9 8 7 6]
+                      [5 4 3 2])
+        b (sut/matrix [2 3 4 5]
+                      [6 7 8 9]
+                      [8 7 6 5]
+                      [4 3 2 1])]
+    (is (not= a b))
+    (is (not (approx a b)))))
+
+(deftest multiplying-two-matrices
+  (let [a (sut/matrix [1 2 3 4]
+                      [5 6 7 8]
+                      [9 8 7 6]
+                      [5 4 3 2])
+        b (sut/matrix [-2 1 2  3]
+                      [ 3 2 1 -1]
+                      [ 4 3 6  5]
+                      [ 1 2 7  8])]
+    (is (= (sut/matrix
+             [20 22  50  48]
+             [44 54 114 108]
+             [40 58 110 102]
+             [16 26  46  42])
+           (sut/mul a b)))))
+
+(deftest a-matrix-multiplied-by-a-tuple
+  (let [a (sut/matrix [1 2 3 4]
+                      [2 4 4 2]
+                      [8 6 4 1]
+                      [0 0 0 1])
+        b (t/tuple 1 2 3 1)]
+    (is (= (t/tuple 18 24 33 1)
+           (sut/mult a b)))))
+
+(deftest identity-makes-identity-matrices
+  (is (= (sut/matrix [1 0]
+                     [0 1])
+         (sut/identity 2)))
+  (is (= (sut/matrix [1 0 0]
+                     [0 1 0]
+                     [0 0 1])
+         (sut/identity 3)))
+  (is (= (sut/matrix [1 0 0 0]
+                     [0 1 0 0]
+                     [0 0 1 0]
+                     [0 0 0 1])
+         (sut/identity 4))))
+
+(deftest multiplying-a-matrix-by-the-identity-matrix
+  (let [a (sut/matrix [0 1  2  4]
+                      [1 2  4  8]
+                      [2 4  8 16]
+                      [4 8 16 32])]
+    (is (= a
+           (sut/mul (sut/identity 4) a)))
+    (is (= a
+           (sut/mul a (sut/identity 4))))))
+
+(deftest multiplying-the-identity-matrix-by-a-tuple
+  (let [a (t/tuple 1 2 3 4)]
+    (is (= a (sut/mult (sut/identity 4) a)))))
+
+(deftest transposing-a-matrix
+  (let [a (sut/matrix [0 9 3 0]
+                      [9 8 0 8]
+                      [1 8 5 3]
+                      [0 0 5 8])]
+    (is (= (sut/matrix [0 9 1 0]
+                       [9 8 8 0]
+                       [3 0 5 5]
+                       [0 8 3 8])
+           (sut/transpose a))))
+
+  (testing "transposing the identity matrix is a noop"
+    (is (= (sut/identity 4)
+           (sut/transpose (sut/identity 4))))))
