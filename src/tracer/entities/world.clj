521ba96e48cc68d27daf7bdaf828130005292b55
Gordon Syme <gordonsyme@gmail.com>
2019-05-15 21:57:58 +0100

    Add intersection and shading fns for worlds

--- src/tracer/entities/world.clj
+++ src/tracer/entities/world.clj
@@ -1,14 +1,17 @@
 (ns tracer.entities.world
   (:require [clojure.spec.alpha :as s]
+            [tracer.entities.colour :as colour]
             [tracer.entities.intersection :as i]
-            [tracer.entities.light :as light]))
+            [tracer.entities.light :as light]
+            [tracer.entities.material :as material]
+            [tracer.entities.ray :as ray]))
 
 (s/def ::world (s/keys :req-un []))
 
 (defn world
   []
-  {:objects #{}
-   :lights #{}})
+  {:objects []
+   :lights []})
 (s/fdef world
   :ret ::world)
 
@@ -41,3 +44,27 @@
   :args (s/cat :w ::world
                :l ::light/light)
   :ret ::world)
+
+(defn intersect
+  [w r]
+  (apply i/intersections (mapcat #(i/intersect % r) (objects w))))
+(s/fdef intersect
+  :args (s/cat :w ::world
+               :r ::ray/ray)
+  :ret ::i/intersections)
+
+(defn shade-hit
+  [w comps]
+  (reduce
+    colour/add
+    (colour/colour 0.0 0.0 0.0)
+    (for [light (lights w)]
+      (material/lighting (:material (:object comps))
+                         light
+                         (:point comps)
+                         (:eyev comps)
+                         (:normalv comps)))))
+(s/fdef shade-hit
+  :args (s/cat :w ::world
+               :comps ::i/computations)
+  :ret ::colour/colour)
--- test/tracer/entities/world_test.clj
+++ test/tracer/entities/world_test.clj
@@ -1,14 +1,16 @@
 (ns tracer.entities.world-test
   (:require [clojure.test :refer (deftest testing is)]
+            [tracer.comparators :refer (approx)]
             [tracer.fixtures :refer (instrument)]
             [tracer.entities.colour :as colour]
             [tracer.entities.light :as light]
+            [tracer.entities.intersection :as i]
             [tracer.entities.material :as material]
             [tracer.entities.sphere :as sphere]
+            [tracer.entities.ray :as ray]
             [tracer.entities.transform :as transform]
             [tracer.entities.tuple :as tup]
-            [tracer.entities.world :as world]
-            ))
+            [tracer.entities.world :as world]))
 
 (clojure.test/use-fixtures :once instrument)
 
@@ -43,6 +45,32 @@
         s2 (sphere/with-transform (sphere/sphere)
                                   (transform/scaling 0.5 0.5 0.5))
         w (default-world)]
-    (is (= #{light} (world/lights w)))
-    (is (contains? (world/objects w) s1))
-    (is (contains? (world/objects w) s2))))
+    (is (= [light] (world/lights w)))
+    (is (contains? (set (world/objects w)) s1))
+    (is (contains? (set (world/objects w)) s2))))
+
+(deftest intersect-a-world-with-a-ray
+  (let [w (default-world)
+        r (ray/ray (tup/point 0 0 -5) (tup/vector 0 0 1))]
+    (is (= [4.0 4.5 5.5 6.0]
+           (map :t (world/intersect w r))))))
+
+(deftest shading-an-intersection
+  (let [w (default-world)
+        shape (first (world/objects w))
+        comps (i/prepare-computations (i/intersection 4 shape)
+                                      (ray/ray (tup/point 0 0 -5)
+                                               (tup/vector 0 0 1)))]
+    (is (approx (colour/colour 0.38066 0.47583 0.2855)
+                (world/shade-hit w comps)))))
+
+(deftest shading-an-intersection-from-the-inside
+  (let [w (assoc (default-world)
+                 :lights [(light/point-light (tup/point 0 0.25 0)
+                                             (colour/colour 1 1 1))])
+        shape (second (world/objects w))
+        comps (i/prepare-computations (i/intersection 0.5 shape)
+                                      (ray/ray (tup/point 0 0 0)
+                                               (tup/vector 0 0 1)))]
+    (is (approx (colour/colour 0.90498 0.90498 0.90498)
+                (world/shade-hit w comps)))))
