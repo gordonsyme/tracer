6007ea394861bb76cd61b0f6cde417d2e80a0baa
Gordon Syme <gordonsyme@gmail.com>
2019-05-20 20:49:49 +0100

    Putting it together: Chapter 8
    Rendering a scene with a camera, multiple spheres, multiple lights, and
    shadows

---
+++ src/putting_together/chapter_8.clj
@@ -0,0 +1,82 @@
+(ns putting-together.chapter-8
+  (:require [clojure.java.io :as io]
+            [tracer.entities.world :as world]
+            [tracer.entities.camera :as camera]
+            [tracer.entities.colour :as colour]
+            [tracer.entities.light :as light]
+            [tracer.entities.tuple :as tup]
+            [tracer.entities.sphere :as sphere]
+            [tracer.entities.material :as material]
+            [tracer.entities.transform :as transform]
+            [tracer.use-cases.ppm :as ppm]))
+
+(defn go
+  [filename]
+  (let [surface-material (-> (material/material)
+                             (material/with-colour (colour/colour 1 0.9 0.9))
+                             (material/with-specular 0))
+        floor (-> (sphere/sphere)
+                  (sphere/with-transform
+                    (transform/scaling 10 0.01 10))
+                  (sphere/with-material surface-material))
+        left-wall (-> (sphere/sphere)
+                      (sphere/with-transform
+                        (-> (transform/identity)
+                            (transform/scale 10 0.01 10)
+                            (transform/rotate-x (/ Math/PI 2))
+                            (transform/rotate-y (- (/ Math/PI 4)))
+                            (transform/translate 0 0 5)))
+                      (sphere/with-material surface-material))
+        right-wall (-> (sphere/sphere)
+                       (sphere/with-transform
+                         (-> (transform/identity)
+                             (transform/scale 10 0.01 10)
+                             (transform/rotate-x (/ Math/PI 2))
+                             (transform/rotate-y (/ Math/PI 4))
+                             (transform/translate 0 0 5)))
+                       (sphere/with-material surface-material))
+        middle-sphere (-> (sphere/sphere)
+                          (sphere/with-transform (transform/translation -0.5 1 0.5))
+                          (sphere/with-material
+                            (-> (material/material)
+                                (material/with-colour (colour/colour 0.1 1 0.5))
+                                (material/with-diffuse 0.7)
+                                (material/with-specular 0.3))))
+        right-sphere (-> (sphere/sphere)
+                         (sphere/with-transform
+                           (-> (transform/scaling 0.5 0.5 0.5)
+                               (transform/translate 1.5 0.5 -0.5)))
+                         (sphere/with-material
+                           (-> (material/material)
+                               (material/with-colour (colour/colour 0.5 1 0.1))
+                               (material/with-diffuse 0.7)
+                               (material/with-specular 0.3))))
+        left-sphere (-> (sphere/sphere)
+                        (sphere/with-transform
+                          (-> (transform/scaling 0.33 0.33 0.33)
+                              (transform/translate -1.5 0.33 -0.75)))
+                        (sphere/with-material
+                          (-> (material/material)
+                              (material/with-colour (colour/colour 1 0.8 0.1))
+                              (material/with-diffuse 0.7)
+                              (material/with-specular 0.3))))
+        world (-> (world/world)
+                  (world/add-light (light/point-light
+                                     (tup/point -10 10 -10)
+                                     (colour/colour 1 1 1)))
+                  (world/add-light (light/point-light
+                                     (tup/point 10 10 -10)
+                                     (colour/colour 0 0 1)))
+                  (world/add-object floor)
+                  (world/add-object left-wall)
+                  (world/add-object right-wall)
+                  (world/add-object middle-sphere)
+                  (world/add-object right-sphere)
+                  (world/add-object left-sphere))
+        camera (camera/with-transform
+                 (camera/camera 1440 720 (/ Math/PI 3))
+                 (transform/view-transform (tup/point 0 1.5 -5)
+                                           (tup/point 0 1 0)
+                                           (tup/vector 0 1 0)))]
+    (with-open [s (io/output-stream (io/as-file filename))]
+      (ppm/generate (camera/render camera world) s))))
