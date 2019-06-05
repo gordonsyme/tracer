4d7cd928bb5ea4cebb18648276686cf76ab87b84
Gordon Syme <gordonsyme@gmail.com>
2019-06-05 21:37:53 +0100

    Add planes

---
+++ src/tracer/entities/plane.clj
@@ -0,0 +1,35 @@
+(ns tracer.entities.plane
+  (:require [clojure.spec.alpha :as s]
+            [tracer.entities.material :as material]
+            [tracer.entities.ray :as ray]
+            [tracer.entities.shape :as shape]
+            [tracer.entities.transform :as transform]
+            [tracer.entities.tuple :as tup]
+            ))
+
+(s/def ::plane ::shape/common)
+
+(defmethod shape/object-type :plane
+  [_]
+  ::plane)
+
+(defn plane
+  []
+  {::shape/tag :plane
+   :transform (transform/identity)
+   :inverse-transform (transform/identity)
+   :material (material/material)})
+(s/fdef plane
+  :ret ::plane)
+
+(defmethod shape/local-intersect :plane
+  [s ray]
+  (if (< (Math/abs (tup/y (ray/direction ray)))
+         0.00001)
+    []
+    [(/ (- (tup/y (ray/origin ray)))
+        (tup/y (ray/direction ray)))]))
+
+(defmethod shape/local-normal-at :plane
+  [s p]
+  (tup/vector 0 1 0))
---
+++ test/tracer/entities/plane_test.clj
@@ -0,0 +1,41 @@
+(ns tracer.entities.plane-test
+  (:require [clojure.test :refer (deftest testing is)]
+            [tracer.fixtures :refer (instrument)]
+            [tracer.entities.plane :as plane]
+            [tracer.entities.ray :as ray]
+            [tracer.entities.shape :as shape]
+            [tracer.entities.tuple :as tup]))
+
+(clojure.test/use-fixtures :once instrument)
+
+(deftest the-normal-of-a-plane-is-constant-everywhere
+  (let [p (plane/plane)]
+    (is (= (tup/vector 0 1 0)
+           (shape/local-normal-at p (tup/point 0 0 0))))
+
+    (is (= (tup/vector 0 1 0)
+           (shape/local-normal-at p (tup/point 10 0 -10))))
+
+    (is (= (tup/vector 0 1 0)
+           (shape/local-normal-at p (tup/point -5 0 150))))))
+
+(deftest plane-intersections
+  (testing "intersect with a ray parallel to the plane"
+    (let [p (plane/plane)
+          r (ray/ray (tup/point 0 10 0) (tup/vector 0 0 1))]
+      (is (empty? (shape/local-intersect p r)))))
+
+  (testing "intersect with a coplanar ray"
+    (let [p (plane/plane)
+          r (ray/ray (tup/point 0 0 0) (tup/vector 0 0 1))]
+      (is (empty? (shape/local-intersect p r)))))
+
+  (testing "a ray intersecting a plane from above"
+    (let [p (plane/plane)
+          r (ray/ray (tup/point 0 1 0) (tup/vector 0 -1 0))]
+      (is (= [1.0] (shape/local-intersect p r)))))
+
+  (testing "a ray intersecting a plane from below"
+    (let [p (plane/plane)
+          r (ray/ray (tup/point 0 -1 0) (tup/vector 0 1 0))]
+      (is (= [1.0] (shape/local-intersect p r))))))
