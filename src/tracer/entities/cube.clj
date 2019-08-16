5b21736ab651ee56faaac040221f362e884eaf5f
Gordon Syme <gordonsyme@gmail.com>
2019-08-16 09:52:37 +0100

    Add cubes

---
+++ src/tracer/entities/cube.clj
@@ -0,0 +1,62 @@
+(ns tracer.entities.cube
+  (:require [clojure.spec.alpha :as s]
+            [tracer.entities.material :as material]
+            [tracer.entities.ray :as ray]
+            [tracer.entities.shape :as shape]
+            [tracer.entities.transform :as transform]
+            [tracer.entities.tuple :as tup]))
+
+(s/def ::cube ::shape/common)
+
+(defmethod shape/object-type :cube
+  [_]
+  ::cube)
+
+(defn cube
+  []
+  {::shape/tag :cube
+   :transform (transform/identity)
+   :inverse-transform (transform/identity)
+   :material (material/material)})
+(s/fdef cube
+  :ret ::cube)
+
+(defn- check-axis
+  [origin direction]
+  (let [tmin-numerator (- -1 ^double origin)
+        tmax-numerator (-  1 ^double origin)
+        ts (if (>= (Math/abs ^double direction)
+                   0.00001)
+             [(/ tmin-numerator ^double direction)
+              (/ tmax-numerator ^double direction)]
+             [(* tmin-numerator Double/POSITIVE_INFINITY)
+              (* tmax-numerator Double/POSITIVE_INFINITY)])]
+    (sort ts)))
+(s/fdef check-axis
+  :args (s/cat :origin double?
+               :direction double?)
+  :ret (s/coll-of double? :count 2))
+
+(defmethod shape/local-intersect :cube
+  [c ray]
+  ;; returns a list of pairs, need max of first, min of second
+  (let [ts (map (fn [axis]
+                  (check-axis (-> ray ray/origin axis)
+                              (-> ray ray/direction axis)))
+                [tup/x tup/y tup/z])
+        tmin (apply max (map first ts))
+        tmax (apply min (map second ts))]
+    (if (> tmin tmax)
+      []
+      [tmin tmax])))
+
+(defmethod shape/local-normal-at :cube
+  [c point]
+  (let [[x y z :as coords] (take 3 point)
+        maxc (apply max (map (fn [^double coord]
+                               (Math/abs coord))
+                             coords))]
+    (cond
+      (= maxc (Math/abs ^double x)) (tup/vector x 0 0)
+      (= maxc (Math/abs ^double y)) (tup/vector 0 y 0)
+      :else (tup/vector 0 0 z))))
---
+++ test/tracer/entities/cube_test.clj
@@ -0,0 +1,55 @@
+(ns tracer.entities.cube-test
+  (:require [clojure.test :refer (deftest testing is are)]
+            [tracer.comparators :refer (approx)]
+            [tracer.fixtures :refer (instrument)]
+            [tracer.entities.intersection :as i]
+            [tracer.entities.cube :as cube]
+            [tracer.entities.ray :as ray]
+            [tracer.entities.shape :as shape]
+            [tracer.entities.tuple :as tup]))
+
+(clojure.test/use-fixtures :once instrument)
+
+(deftest a-ray-intersects-a-cube
+  (let [c (cube/cube)]
+    (are [origin direction t1 t2]
+         (let [r (ray/ray origin direction)
+               xs (i/intersect c r)]
+           (= [(double t1) (double t2)]
+              (map :t xs)))
+      ;; +/- direction vectors
+      (tup/point  5    0.5  0) (tup/vector -1  0  0) 4 6
+      (tup/point -5    0.5  0) (tup/vector  1  0  0) 4 6
+      (tup/point  0.5  5    0) (tup/vector  0 -1  0) 4 6
+      (tup/point  0.5 -5    0) (tup/vector  0  1  0) 4 6
+      (tup/point  0.5  0    5) (tup/vector  0  0 -1) 4 6
+      (tup/point  0.5  0   -5) (tup/vector  0  0  1) 4 6
+      ;; a point inside the cube
+      (tup/point 0 0.5 0) (tup/vector 0 0 1) -1 1)))
+
+(deftest a-ray-misses-a-cube
+  (let [c (cube/cube)]
+    (are [origin direction]
+         (let [r (ray/ray origin direction)
+               xs (i/intersect c r)]
+           (empty? xs))
+      ;; +/- direction vectors
+      (tup/point -2  0  0) (tup/vector 0.2673 0.5345 0.8018)
+      (tup/point  0 -2  0) (tup/vector 0.8018 0.2673 0.5345)
+      (tup/point  0  0 -2) (tup/vector 0.5345 0.8018 0.2673)
+      (tup/point 2 0 2) (tup/vector  0  0 -1)
+      (tup/point 0 2 2) (tup/vector  0 -1  0)
+      (tup/point 2 2 0) (tup/vector -1  0  0 ))))
+
+(deftest the-normal-on-the-surface-of-a-cube
+  (let [c (cube/cube)]
+    (are [point normal]
+         (approx normal (shape/local-normal-at c point))
+      (tup/point  1    0.5 -0.8) (tup/vector  1  0 0)
+      (tup/point -1   -0.2  0.9) (tup/vector -1  0 0)
+      (tup/point -0.4  1   -0.1) (tup/vector  0  1 0)
+      (tup/point  0.3 -1   -0.7) (tup/vector  0 -1 0)
+      (tup/point -0.6  0.3  1) (tup/vector  0 0  1)
+      (tup/point  0.4  0.4 -1) (tup/vector  0 0 -1)
+      (tup/point  1    1    1) (tup/vector  1 0  0)
+      (tup/point -1   -1   -1) (tup/vector -1 0  0))))
