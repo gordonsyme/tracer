cacd23217d36ff25d4499522c2255f3595ce54e1
Gordon Syme <gordonsyme@gmail.com>
2020-05-10 15:35:36 +0100

    Refresh test dependencies, add coverage alias

--- project.clj
+++ project.clj
@@ -2,11 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :profiles {:dev {:dependencies [[orchestra "2018.12.06-2"]
-                                  [org.clojure/test.check "0.10.0-alpha4"]
+  :profiles {:dev {:dependencies [[orchestra "2019.02.06-1"]
+                                  [org.clojure/test.check "1.0.0"]
                                   [criterium "0.4.5"]]}
              :fast {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]
                     :global-vars {*unchecked-math* true}}}
   :plugins [[lein-cloverage "1.1.1"]]
+  :aliases {"coverage" ["cloverage" "--ns-exclude-regex" "putting-together\\..*"]}
   :repl-options {:init-ns tracer.core}
   :global-vars {*warn-on-reflection* true})
